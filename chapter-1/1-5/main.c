de765317a4775ee43a4954157dfd768cc99769d4
AliHMohammad <alihmohammad@hotmail.com>
2024-08-29 11:51:37 +0200

    feat: updated 1-5

--- chapter-1/1-5/main.c
+++ chapter-1/1-5/main.c
@@ -8,9 +8,9 @@
 int main(int argc, char const *argv[])
 {
     /* code */
-    for (int fahr = 300; fahr >= 0; fahr -= 20)
-    {
-        printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+    for (int fahr = 300; fahr >= 0; fahr -= 20) {
+        float celcius = (5.0 / 9.0) * (fahr - 32);
+        printf("%3d %6.1f\n", fahr, celcius);
     }
 
     return 0;
